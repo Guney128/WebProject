351891a7fad7dd85c1a693e1f60663926a3326c9
soha <92631919+sohakh1n@users.noreply.github.com>
2021-12-28 23:44:15 +0100

    Add files via upload

---
+++ css/about.php
@@ -0,0 +1,40 @@
+
+<?php
+    include_once 'header.php';
+?>
+<!-- The video -->
+<video autoplay muted loop id="myVideo">
+  <source src="./vid/city.mp4" type="video/mp4">
+</video>
+
+<!-- Optional: some overlay text to describe the video -->
+<div class="content">
+  <h1>About us</h1>
+ 
+  <!-- Use a button to pause/play the video with JavaScript -->
+  <button id="myBtn" onclick="myFunction()">Pause</button>
+</div>
+<script>
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+</script>
+
+
+<?php
+    include_once 'footer.php';
+?>
+ 
