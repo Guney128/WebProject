be29cb02e93bc27bbcdeb8200235ef52761086db
soha <92631919+sohakh1n@users.noreply.github.com>
2022-01-03 09:38:52 +0100

    Add files via upload

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php
+class DATABASE {    
+    public function dbConnect() {        
+        static $DBH = null;      
+        if (is_null($DBH)) {              
+			$connection = new mysqli(HOST, USER, PASSWORD, DATABASE);			
+			if($connection->connect_error){
+				die("Error failed to connect to MySQL: " . $connection->connect_error);
+			} else {
+				$DBH = $connection;
+			}         
+        }
+        return $DBH;    
+    }     
+}
