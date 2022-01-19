39fd1be057eed457dd0ef22f11b4b653f93e35fb
soha <92631919+sohakh1n@users.noreply.github.com>
2022-01-19 09:38:42 +0100

    Add files via upload

--- help.php
+++ help.php
@@ -8,29 +8,32 @@
 </div>
 
         </section>
-
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="./css/style.css">
+        
+      <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <!-- FontAwesome CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.css">
+<link rel="stylesheet" href="./css/styles.css">
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
 
 <div class="text-center">
     <h2 class="mt-5 mb-5">FAQ</h2>
   </div>
   <section class="container my-5" id="maincontent">
     <section id="accordion">
+
+
       <a class="py-3 d-block h-100 w-100 position-relative z-index-1 pr-1 text-secondary border-top" aria-controls="faq-17" aria-expanded="false" data-toggle="collapse" href="#faq-17" role="button">
         <div class="position-relative">
           <h2 class="h4 m-0 pr-3">
           What is "SIGN UP" for?
           </h2>
           <div class="position-absolute top-0 right-0 h-100 d-flex align-items-center">
-           
+          <i class="fa fa-plus"></i>
           </div>
         </div>
       </a>
@@ -47,7 +50,7 @@
              How do i log into my account?
           </h2>
           <div class="position-absolute top-0 right-0 h-100 d-flex align-items-center">
-           
+          <i class="fa fa-plus"></i>
           </div>
         </div>
       </a>
@@ -64,7 +67,7 @@
           How do I update my guest information online?
           </h2>
           <div class="position-absolute top-0 right-0 h-100 d-flex align-items-center">
-            
+          <i class="fa fa-plus"></i> 
           </div>
         </div>
       </a>
@@ -81,7 +84,7 @@
           Where can I find maps and directions to the hotel?
           </h2>
           <div class="position-absolute top-0 right-0 h-100 d-flex align-items-center">
-           
+          <i class="fa fa-plus"></i>
           </div>
         </div>
       </a>
@@ -98,7 +101,7 @@
           What is "TICKETS" for? 
           </h2>
           <div class="position-absolute top-0 right-0 h-100 d-flex align-items-center">
-           
+          <i class="fa fa-plus"></i>
           </div>
         </div>
       </a>
@@ -115,37 +118,38 @@
           How do I write a ticket? 
           </h2>
           <div class="position-absolute top-0 right-0 h-100 d-flex align-items-center">
-           
+          <i class="fa fa-plus"></i>
           </div>
         </div>
       </a>
       <div class="collapse" id="faq-22">
         <div class="card card-body border-0 p-0">
           <p> 1. Login your account. <br>
-2. Upload your issue and the regarding picture under „create ticket“.  <br>
-3. Wait for the response.  <br>
-</p>
+          2. Upload your issue and the regarding picture under „create ticket“.  <br>
+          3. Wait for the response.  <br>
+        </p>
         </div>
       </div>
 
 
-      <a class="py-3 d-block h-100 w-100 position-relative z-index-1 pr-1 text-secondary  border-top" aria-controls="faq-20" aria-expanded="false" data-toggle="collapse" href="#faq-20" role="button">
+    
+      <a class="py-3 d-block h-100 w-100 position-relative z-index-1 pr-1 text-secondary  border-top" aria-controls="faq-16" aria-expanded="false" data-toggle="collapse" href="#faq-16" role="button">
         <div class="position-relative">
           <h2 class="h4 m-0 pr-3">
           How do I contact your hotel if my question isn’t answered here?
           </h2>
           <div class="position-absolute top-0 right-0 h-100 d-flex align-items-center">
-           
+          <i class="fa fa-plus"></i>
           </div>
         </div>
       </a>
-      <div class="collapse" id="faq-20">
+      <div class="collapse" id="faq-16">
         <div class="card card-body border-0 p-0">
           <p>You can contact us via e-mail or phone for any inquiries. You can find all of our contact information under "Imprint".</p>
          
         </div>
       </div>
-
+ 
 
     </section>
   </section>
